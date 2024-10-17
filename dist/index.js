3780d9f5aa5c626b408e74ac530c972ce545f845
Sergey <seleznev-krista@mail.ru>
2024-10-17 18:25:54 +0300

    init: init project

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import http from "http";
+const server = http.createServer(function (request, response) {
+    response.end("Hello METANIT.COM!");
+});
+server.listen(3000, function () { console.log("Сервер запущен по адресу http://localhost:3000"); });
